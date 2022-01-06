72943dc23bae9d96961ecc0abdb0d9e17f5b6e72
Teddy Crane <teddy.crane@slalom.com>
2022-01-06 11:46:42 -0800

    Update App Bar and App Title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
-import { AppBar, Container, Link, Menu, Toolbar } from "@mui/material";
+import { AppBar, Container, Link, Toolbar } from "@mui/material";
 import React from "react";
 
 export const Header = (): JSX.Element => {
   return (
     <AppBar position="static" data-testid="race-management-header">
-      <Container maxWidth="xl">
+      <Container maxWidth="xl" disableGutters>
         <Toolbar disableGutters>
           <Link href="/" variant="h6" color="inherit">
             Race Management
